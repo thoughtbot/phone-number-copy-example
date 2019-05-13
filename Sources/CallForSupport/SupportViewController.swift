5a5f1e731a67c0356b77c47bd07614035102dc98
Adam Sharp <adam@sharplet.me>
2019-05-13 14:55:10 -0400

    Call phone number and clear selection

--- Sources/CallForSupport/SupportViewController.swift
+++ Sources/CallForSupport/SupportViewController.swift
@@ -7,12 +7,25 @@ final class SupportViewController: UITableViewController {
     SupportRegion(regionCodes: ["GB"], phoneNumber: PhoneNumber("+44-800-123-4567")),
   ]
 
+  private var isOpeningPhoneURL = false
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    NotificationCenter.default.addObserver(
+      self,
+      selector: #selector(clearSelection),
+      name: UIApplication.didBecomeActiveNotification,
+      object: nil
+    )
+  }
+
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return regions.count
   }
 
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let region = self.regions[indexPath.row]
+    let region = regions[indexPath.row]
     let cell = tableView.dequeueReusableCell(withIdentifier: "Support Region", for: indexPath)
     cell.textLabel?.text = region.localizedName
     cell.detailTextLabel?.text = region.phoneNumber.rawValue
@@ -20,6 +33,20 @@ final class SupportViewController: UITableViewController {
   }
 
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    let region = regions[indexPath.row]
+
+    UIApplication.shared.open(region.phoneNumber.url) { success in
+      if success {
+        self.isOpeningPhoneURL = true
+      } else {
+        // TODO: handle phone call failure
+      }
+    }
+  }
+
+  @objc func clearSelection() {
+    guard let indexPath = tableView.indexPathForSelectedRow, isOpeningPhoneURL else { return }
+    isOpeningPhoneURL = false
     tableView.deselectRow(at: indexPath, animated: true)
   }
 }
